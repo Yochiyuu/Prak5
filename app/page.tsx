a451fe8ed1c0bae4de129166bffe39aa3deeb8ce
Andrew Reinhart <ndrurei@gmail.com>
2025-11-13 12:26:51 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Image from "next/image";
+
+// Components
+import ContactSection from "../components/contactsection";
+import Footer from "../components/footer";
+import HeroSection from "../components/herosection";
+import Navbar from "../components/navbar";
+import ServiceSection from "../components/servicesection";
+import WorkSection from "../components/worksection";
+
+// Bootstrap
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <HeroSection></HeroSection>
+        <Container className="my-5">
+          <Row className="my-4">
+            <Col md={6} className="align-self-center mb-3">
+              <h2>About Us</h2>
+              <h6>What We Do?</h6>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                assumenda, necessitatibus veniam vitae deleniti, quos nulla
+                obcaecati eligendi nihil hic cum ex. Dolor ab eum dolore earum
+                magni aut quis.
+              </p>
+              <Button variant="info">Learn More</Button>
+            </Col>
+            <Col md={6}>
+              <p>
+                <Image
+                  src="/images/about.jpg"
+                  alt="About Us"
+                  width={500}
+                  height={500}
+                ></Image>
+              </p>
+            </Col>
+          </Row>
+        </Container>
+        <section className="bg-dark">
+          <WorkSection></WorkSection>
+        </section>
+        <ServiceSection></ServiceSection>
+        <section id="contact">
+          <ContactSection></ContactSection>
+        </section>
+        <Footer></Footer>
+      </main>
+    </div>
+  );
+}
