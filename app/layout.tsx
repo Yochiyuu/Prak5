5d86c83df32a3655f51da775ada54cc7bf84744b
Andrew Reinhart <ndrurei@gmail.com>
2025-11-21 20:33:22 +0700

    fix: add bootstrap css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import { Providers } from "./providers";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const poppins = Poppins({
   subsets: ["latin"],
